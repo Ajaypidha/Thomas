7dc58dff8a9ad9ac6cc728ba23fff4ce1fc4f3ae
Abhishek Mishra <mobappssolutions149@gmail.com>
2022-09-14 11:46:28 +0530

    first commit

--- Thomasapp/models.py
+++ Thomasapp/models.py
@@ -5,15 +5,7 @@ from django.db import models
 class Student(models.Model):
     name=models.CharField(null=True,blank=True)
     heading=models.CharField(null=True,blank=True)
-    description1=models.CharField(null=True,blank=True)
-    description2=models.CharField(null=True,blank=True)
-    description3=models.CharField(null=True,blank=True)
-    description4=models.CharField(null=True,blank=True)
-    school=models.CharField(null=True,blank=True)
-
-    product=models.CharField(null=True,blank=True)
-    apple=models.CharField(null=True,blank=True)
-    bisk=models.CharField(null=True,blank=True)
+   
       
 
       
